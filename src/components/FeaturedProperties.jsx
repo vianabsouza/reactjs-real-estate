fb89284af04fbf040183622075d7911e994dd344
Barbara Viana <vianabsouza@gmail.com>
2024-11-02 05:25:39 -0300

    fix: add unique key to PropertyCard component in FeaturedProperties

--- src/components/FeaturedProperties.jsx
+++ src/components/FeaturedProperties.jsx
@@ -9,7 +9,7 @@ function FeaturedProperties({ properties }) {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-12">
           {properties.map((property) => (
-            <PropertyCard {...property} />
+            <PropertyCard key={property.id} {...property} />
           ))}
         </div>
       </div>
